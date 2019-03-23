5591cf43d403c3f329c01422cabebfd448c66469
michjadams <Mjadams66@gmail.com>
2019-03-22 22:26:07 -0400

    fighting computer to use python3 instead of 2, switching to desktop

---
+++ chapter001/convex_hull_test.py
@@ -0,0 +1,13 @@
+# import unittest
+# from convex_hull.py import *
+#
+# class convex_hull_test(unittest.testcase):
+#     def creates_find_convex_hull(self):
+#         self.assertEqual(convex_hull([]),[])
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
