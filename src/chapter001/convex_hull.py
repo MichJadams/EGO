3e7680cea91b95fcf03908b2f1b01682ce4acd04
michjadams <Mjadams66@gmail.com>
2019-03-24 11:01:34 -0400

    creates tests for convex hull algo, begins to implement nieve solution

---
+++ src/chapter001/convex_hull.py
@@ -0,0 +1,43 @@
+def findConvexHull(arrayOfPoints):
+    if len(arrayOfPoints):
+        return arrayOfPoints
+
+    memoCheckedTriangles = {}
+    memoCheckPoints = {}
+    externalPoints = []
+    for pointOne in arrayOfPoints:
+
+        for pointTwo in arrayOfPoints:
+            if pointTwo == pointOne:
+                continue
+
+            for pointThree in arrayOfPoints:
+                if pointThree == pointTwo:
+                    continue
+                for point in arrayOfPoints:
+
+                    triangle = [pointOne, pointTwo, pointThree].sort()
+                    if triangle not in memoCheckedTriangles:
+                        memoCheckedTriangles[triangle] = True
+                        if point not in memoCheckPoints
+                        && point != pointOne
+                        && point != pointTwo
+                        && point != pointThree:
+                        #if the point is not part of the triangle being check
+                        # and it has not already been checked
+                        # check it
+                            isContained = triangleContains(point, [pointOne,pointTwo,pointThree])
+                            memoCheckPoints[point] = isContained
+                        else:
+                            continue
+                    else:
+                        continue
+
+    for point in memoCheckPoints:
+        if memoCheckPoints[point] == True:
+            externalPoints.push(point)
+    return externalPoints
+
+def isContained(point, triangle):
+# checks if a point is contained in a triangle
+    
--- src/chapter001/convex_hull_test.py
+++ src/chapter001/convex_hull_test.py
@@ -1,16 +1,29 @@
 import unittest
 import convex_hull
 
-print("running tests")
-
 class test_convex_hull_test(unittest.TestCase):
-    def test_setUp(self):
+    def setUp(self):
         try:
-            self.test = convex_hull.findConvexHull()
+            self.test = convex_hull.findConvexHull
+            self.points = [[4,2],[1,3],[4,5],[2,5],[6,4],[4,7],[2,8]]
+            self.convexHullOutlinePoints = [[6,4],[4,7],[2,8],[1,3],[4,2]]
+            self.containedPoints = [[4,5],[2,5]]
         except NameError as e:
             pass
     def test_creates_find_convex_hull(self):
-        self.assertEqual(self.test([]),[])
+        self.assertEqual(self.test([]),[], msg="returns an empty array when envoked with an empty array")
+    def test_less_than_three_points_passed_in(self:
+        self.assertEqual(self.test([[1,2],[2,2]]),[1,2],[2,2])
+    def test_passed_three_points_returns_those(self):
+        self.assertEqual(self.test([[1,2],[2,2],[4,5]]), [[1,2],[2,2],[4,5]])
+    def test_includes_points_along_convex_hull(self):
+        result = self.test(self.points)
+        for point in self.convexHullOutlinePoints:
+            self.assertIn(point, result, msg="external coordinate, should be included in outline")
+    def test_does_not_include_points_inside_convex_hull(self):
+        result = self.test(self.points)
+        for point in self.containedPoints:
+            self.assertNotIn(point, result, msg="internal coordinate, should not be included in outline")
 
 if __name__ == '__main__':
     unittest.main()
