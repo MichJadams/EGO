ca4b9e5b9e3c7d174e5be31f2da2271e4e8922ab
michjadams <Mjadams66@gmail.com>
2019-04-13 13:46:50 -0400

    creates insertion sort tests and class

---
+++ src/insertionSort/insertion_sort.py
@@ -0,0 +1,16 @@
+class insertion_sorter:
+    def __init__(self, array):
+        self.arrayToSort = array
+        self.sortedStates = []
+
+    def insertionSort(self):
+        for index, value in enumerate(self.arrayToSort):
+            self.insert(self.arrayToSort, index, value)
+        return self.arrayToSort
+
+    def insert(self,array,index, value):
+        index -= 1
+        while index >= 0 and array[index] > value:
+            array[index + 1] = array[index]
+            index -= 1
+        array[index + 1] = value
--- src/insertionSort/insertion_sort_test.py
+++ src/insertionSort/insertion_sort_test.py
@@ -1,11 +1,25 @@
 import unittest
 import insertion_sort
 
-class insertion_sort_test(unittest):
+class insertion_sort_test(unittest.TestCase):
     def setUp(self):
         try:
-            self.testClass = insertion_sort.insertionSort()
+            self.testClass = insertion_sort.insertion_sorter([])
         except NameError as e:
             pass
+    def test_returns_empty_array(self):
+        self.testClass = insertion_sort.insertion_sorter([])
+        result = self.testClass.insertionSort()
+        failMsg = "insertionSort does not return an empty array when passed one"
+        self.assertEqual(0, len(result), failMsg)
+
+    def test_sorts_small_array(self):
+        self.testClass = insertion_sort.insertion_sorter([1,5,4])
+        result = self.testClass.insertionSort()
+        expected = [1,4,5]
+        for index in range(0,len(result)):
+            failMsg = "incorrect value at {0}".format(index)
+            self.assertEqual(expected[index],result[index], failMsg)
+
 if __name__ == '__main__':
     unittest.main()
