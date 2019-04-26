f6d599122a840fb8b99ed7cae08aef4a0ae22851
michjadams <Mjadams66@gmail.com>
2019-04-26 07:49:59 -0400

    created a test for the intermediate states of insertion sort

--- src/insertionSort/insertion_sort.py
+++ src/insertionSort/insertion_sort.py
@@ -6,6 +6,7 @@ class insertion_sorter:
     def insertionSort(self):
         for index, value in enumerate(self.arrayToSort):
             self.insert(self.arrayToSort, index, value)
+            self.sortedStates.append(self.arrayToSort.copy())
         return self.arrayToSort
 
     def insert(self,array,index, value):
@@ -13,4 +14,5 @@ class insertion_sorter:
         while index >= 0 and array[index] > value:
             array[index + 1] = array[index]
             index -= 1
+
         array[index + 1] = value
--- src/insertionSort/insertion_sort_test.py
+++ src/insertionSort/insertion_sort_test.py
@@ -7,6 +7,7 @@ class insertion_sort_test(unittest.TestCase):
             self.testClass = insertion_sort.insertion_sorter([])
         except NameError as e:
             pass
+
     def test_returns_empty_array(self):
         self.testClass = insertion_sort.insertion_sorter([])
         result = self.testClass.insertionSort()
@@ -20,6 +21,19 @@ class insertion_sort_test(unittest.TestCase):
         for index in range(0,len(result)):
             failMsg = "incorrect value at {0}".format(index)
             self.assertEqual(expected[index],result[index], failMsg)
+
+    def test_stores_correct_intermediate_sorted_states(self):
+        self.testClass = insertion_sort.insertion_sorter([1,4,3,2,7,5,6,9])
+        result = self.testClass.insertionSort()
+        actualIntermediatStates = self.testClass.sortedStates
+        expectedIntermediateStates = [[1,4,3,2,7,5,6,9],[1,4,3,2,7,5,6,9],
+        [1,3,4,2,7,5,6,9],[1,2,3,4,7,5,6,9],[1,2,3,4,7,5,6,9],[1,2,3,4,5,7,6,9],[1,2,3,4,5,6,7,9],[1,2,3,4,5,6,7,9]]
+        self.assertEqual(len(expectedIntermediateStates), len(actualIntermediatStates),
+        msg="The expected and the actual intermediate states are of different lengths")
+        
+        for arrayIndex in range(0, len(expectedIntermediateStates)):
+            for numberIndex in range(0, len(expectedIntermediateStates)):
+                self.assertEqual(expectedIntermediateStates[arrayIndex][numberIndex],actualIntermediatStates[arrayIndex][numberIndex])
 
 if __name__ == '__main__':
     unittest.main()
